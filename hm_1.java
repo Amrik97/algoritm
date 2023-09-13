a0ce7c05da459ddadb8d14c4d30e746e15910235
Amirkhan <amirhan75@gmail.com>
2023-09-13 17:02:29 +0600

    first commit

---
+++ hm_1.java
@@ -0,0 +1,68 @@
+public class hm_1 {
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+
+        // Построение максимальной кучи
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // Извлечение элементов из кучи один за другим
+        for (int i = n - 1; i >= 0; i--) {
+            // Перемещаем текущий корень в конец массива
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            // Вызываем heapify() на уменьшенной куче
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void heapify(int[] arr, int n, int i) {
+        int largest = i; // Инициализируем наибольший элемент как корень
+        int left = 2 * i + 1; // Левый дочерний элемент
+        int right = 2 * i + 2; // Правый дочерний элемент
+
+        // Если левый дочерний элемент больше корня
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+
+        // Если правый дочерний элемент больше наибольшего элемента на данный момент
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        // Если наибольший элемент не корень
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            // Рекурсивно heapify на поддереве
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+        int n = arr.length;
+
+        System.out.println("Исходный массив:");
+        printArray(arr);
+
+        heapSort(arr);
+
+        System.out.println("Отсортированный массив:");
+        printArray(arr);
+    }
+}
